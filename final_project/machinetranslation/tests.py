305528ae0a46b599439841445da70ec2e176a5e9
ivanbalap <47894209+ivanbalap@users.noreply.github.com>
2023-07-22 00:22:58 +0700

    Add files via upload

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,3 +1,6 @@
+'''
+Test script for translator
+'''
 import unittest
 from translator import englishToFrench, frenchToEnglish
 
@@ -8,12 +11,24 @@ ENGLISH_TEXT_2="Thank you"
 FRENCH_TEXT_2="Mercy"
 
 class TestEnglishToFrench(unittest.TestCase):
+    '''
+    Class: English to French testing 
+    '''
     def test1(self):
+        '''
+        Test 1 for E2F
+        '''
         self.assertEqual(englishToFrench(ENGLISH_TEXT_1),FRENCH_TEXT_1)
         self.assertNotEqual(englishToFrench(ENGLISH_TEXT_2),FRENCH_TEXT_1)
 
 class TestFrenchToEnglish(unittest.TestCase):
+    '''
+    Class: French to English testing 
+    '''
     def test1(self):
+        '''
+        Test 1 for F2E
+        '''
         self.assertEqual(frenchToEnglish(FRENCH_TEXT_1),ENGLISH_TEXT_1)
         self.assertNotEqual(frenchToEnglish(FRENCH_TEXT_2),ENGLISH_TEXT_1)
 
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,16 +1,25 @@
+'''
+Python package using deep_translator
+'''
 from deep_translator import MyMemoryTranslator
 
-def englishToFrench(englishText):
-    frenchText = MyMemoryTranslator(source='english', target='french').translate(englishText)
+def englishToFrench(english_text):
+    '''
+    Function: translating English to French
+    '''
+    ret_french_text = MyMemoryTranslator(source='english', target='french').translate(english_text)
 
-    return frenchText
+    return ret_french_text
 
-def frenchToEnglish(frenchText):
-    englishText = MyMemoryTranslator(source='french', target='english').translate(frenchText)
+def frenchToEnglish(french_text):
+    '''
+    Function: translating French to English
+    '''
+    ret_english_text = MyMemoryTranslator(source='french', target='english').translate(french_text)
 
-    return englishText
+    return ret_english_text
 
 if __name__ == '__main__':
-    mText="I want to watch a movie"
-    print(englishToFrench(mText))
-    print(frenchToEnglish(englishToFrench(mText)))
+    MY_TEXT="I want to watch a movie"
+    print(englishToFrench(MY_TEXT))
+    print(frenchToEnglish(englishToFrench(MY_TEXT)))
